2479ebf75aa7b1b4c191e19df0a65d1f3bcb991e
yangkai <408786028@qq.com>
2018-12-05 09:45:02 +0800

    2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import {} from 'antd-mobile';
 
-ReactDOM.render(<Button type="primary">按钮</Button> ,document.getElementById('root'));
+ReactDOM.render(<Button type="primary">按钮</Button>
+  ,document.getElementById('root'));
 
 
